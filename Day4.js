8c7c419d8eb80bb3609e76a498f73c77be147d6c
F20RSEEN1M01023H <choudharyyhammad@gmail.com>
2026-07-20 12:39:40 +0500

    Hammad improves the async and await

--- Day4.js
+++ Day4.js
@@ -13,3 +13,36 @@ loginUser
     .then((message) => console.log(message))
     .catch((error) => console.log(error)
     )
+
+
+const getStudentResult = new Promise((resolve, reject) => {
+    const marks = 85;
+
+    if (marks > 50) {
+        resolve("You passed")
+    } else {
+        reject("You failed")
+    }
+})
+
+getStudentResult
+    .then((message) => console.log(message)
+    )
+    .catch((error) => console.log(error)
+    )
+
+
+const getStudentResultAsync = async () => {
+
+
+    try {
+        const message = await getStudentResult;
+        console.log(message);
+
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+}
+getStudentResultAsync();
