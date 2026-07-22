bdb5e7e40c8a73c94a23d4ab44226b7030348ac2
F20RSEEN1M01023H <choudharyyhammad@gmail.com>
2026-07-22 12:14:50 +0500

    Hammad updates now

--- Day6.js
+++ Day6.js
@@ -5,7 +5,7 @@
 
 // String method 
 // 1.length()
-const name="Hammad Ashraf";
+const name = "Hammad Ashraf";
 console.log(name.length);
 
 // 2.toUpperCase()
@@ -16,5 +16,48 @@ console.log(name.toUpperCase());
 console.log(name.toLowerCase());
 
 // .includes()
-const email="hammadchoudharyy@gmail.com"
+const email = "hammadchoudharyy@gmail.com"
 console.log(email.includes("@"));
+
+// startsWith 
+const url = "https://google.com"
+console.log(url.startsWith("https"));
+
+// endsWith()
+console.log(url.endsWith("m"));
+
+// indexOf()
+console.log(url.indexOf("e"));
+
+// Slice 
+
+console.log(url.slice(1, 6));
+
+
+// replace 
+console.log(name.replace("Hammad", "MAAZ"));
+
+// replaceAll()
+const animals = "cat dog cat cow goat"
+console.log(animals.replaceAll("cat","Sparrow"));
+
+const trimVar="      UMER AND HAMMAD INTERS    ";
+console.log(trimVar.trim());
+
+const skills="HTML5, CSS3, REACT";
+console.log(skills.split(","));
+
+// charAt()
+console.log(skills.charAt(3));
+
+// concat()
+console.log(skills.concat(" ", trimVar));
+
+// repeat()
+console.log("JavaScript".repeat(3));
+
+
+
+
+
+// Problems practice 
