4fc082fa05f48f098336ebcc3c8812e805f95cc6
F20RSEEN1M01023H <choudharyyhammad@gmail.com>
2026-07-22 10:22:53 +0500

    Hammad is here and updated the code

---
+++ Day6.js
@@ -0,0 +1,20 @@
+// *****************************String Methods *****************************
+// Strings are the text only.
+// ex: like string is a person and the skills of that person are the string methods 
+// like walking, running and etc 
+
+// String method 
+// 1.length()
+const name="Hammad Ashraf";
+console.log(name.length);
+
+// 2.toUpperCase()
+console.log(name.toUpperCase());
+
+
+// .toLowerCase() 
+console.log(name.toLowerCase());
+
+// .includes()
+const email="hammadchoudharyy@gmail.com"
+console.log(email.includes("@"));
