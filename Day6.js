dff4beb25ba83e2423ac972a23a667d673afde5f
F20RSEEN1M01023H <choudharyyhammad@gmail.com>
2026-07-23 16:33:34 +0500

    Hammad is here and working on the js

--- Day6.js
+++ Day6.js
@@ -39,12 +39,12 @@ console.log(name.replace("Hammad", "MAAZ"));
 
 // replaceAll()
 const animals = "cat dog cat cow goat"
-console.log(animals.replaceAll("cat","Sparrow"));
+console.log(animals.replaceAll("cat", "Sparrow"));
 
-const trimVar="      UMER AND HAMMAD INTERS    ";
+const trimVar = "      UMER AND HAMMAD INTERS    ";
 console.log(trimVar.trim());
 
-const skills="HTML5, CSS3, REACT";
+const skills = "HTML5, CSS3, REACT";
 console.log(skills.split(","));
 
 // charAt()
@@ -61,3 +61,95 @@ console.log("JavaScript".repeat(3));
 
 
 // Problems practice 
+
+const username = " Hammad Ashraf ";
+console.log(username.trim().toUpperCase());
+
+const email2 = "HammadAshraf@GMAIL.COM";
+const check = email.toLowerCase().includes("@")
+if (check) {
+    console.log("Valid Email");
+
+}
+else {
+    console.log("Invalid Email");
+}
+
+const sentence = "HTML,CSS,JavaScript,React";
+
+const convert = sentence.split(",");
+for (const c of convert) {
+    console.log(c);
+
+}
+
+const fileName = "resume.pdf";
+const pdfCheck = fileName.endsWith(".pdf")
+if (pdfCheck) {
+    console.log("PDF file accepted");
+
+} else {
+    console.log("Invalid File");
+}
+console.log();
+
+
+
+// Number Methods 
+// 1-toFixed()
+
+const price = 999.4567;
+console.log(price.toFixed(2));
+
+// 2. toString()
+
+// Converts a number into a string.
+
+const age = 23;
+
+console.log(typeof (age.toString()), age.toString());
+
+// 9. Math.floor()
+
+// Always rounds down.
+
+console.log(Math.floor(4.9));
+
+// 10. Math.ceil()
+
+// Always rounds up.
+
+console.log(Math.ceil(4.1));
+
+// 11. Math.random()
+
+// Generates a random decimal between 0 and 1.
+
+console.log(Math.random());
+
+
+
+// problems to solve 
+const price2 = 1999.9876;
+
+console.log(price2.toFixed(2));
+
+const age2 = "23";
+const weight = "72.8kg";
+
+console.log(Number(age2) + 5);
+
+console.log(parseFloat(weight));
+
+
+const number = 8.67;
+
+console.log(Math.round(number));
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+
+const integ = 25.5;
+console.log(Number.isInteger(integ));
+
+const random = Math.floor(Math.random() * 6) + 1;
+console.log(random);
---
+++ Day7.js
@@ -0,0 +1,11 @@
+// ************************Spread vs Rest Operator 
+// spread is the expanding and rest is the collecting the values 
+
+// =>Spread 
+const numbers=[10,102,203,45,566,354]
+console.log(...numbers);
+
+console.log(typeof (numbers));
+
+// =>Rest 
+const fruits=["apple","Bnanana"]
